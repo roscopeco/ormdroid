a417cacec43ce633448533a642d1d965c2acc647
Ross Bamford <roscopeco@gmail.com>
2012-08-23 15:16:43 +0000

    Log SQL when fetching linked entities

--- ORMDroid/src/com/roscopeco/ormdroid/EntityTypeMapping.java
+++ ORMDroid/src/com/roscopeco/ormdroid/EntityTypeMapping.java
@@ -15,10 +15,11 @@
  */
 package com.roscopeco.ormdroid;
 
-import com.roscopeco.ormdroid.Entity.EntityMapping;
-
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.roscopeco.ormdroid.Entity.EntityMapping;
 
 /**
  * Standard mapping for {@link Entity} subclasses. Automatically saves the
@@ -30,6 +31,8 @@ import android.database.sqlite.SQLiteDatabase;
  * @author rosco
  */
 public class EntityTypeMapping implements TypeMapping {
+  private static final String TAG = "EntityTypeMapping";
+  
   public Class<?> javaType() {
     return Entity.class;
   }
@@ -66,9 +69,12 @@ public class EntityTypeMapping implements TypeMapping {
     if (Entity.class.isAssignableFrom(expectedType)) {
       @SuppressWarnings("unchecked")
       Class<? extends Entity> expEntityType = (Class<? extends Entity>)expectedType;
-      
+
+      // TODO could use Query here? Maybe Query could have a primaryKey() method to select by prikey?
       EntityMapping map = Entity.getEntityMappingEnsureSchema(db, expEntityType);
-      Cursor valc = db.rawQuery("SELECT * FROM " + map.mTableName + " WHERE " + map.mPrimaryKeyColumnName + "=?", new String[] { ""+c.getInt(columnIndex) });
+      String sql = "SELECT * FROM " + map.mTableName + " WHERE " + map.mPrimaryKeyColumnName + "=" + c.getInt(columnIndex) + " LIMIT 1";
+      Log.v(TAG, sql);
+      Cursor valc = db.rawQuery(sql, null);
       if (valc.moveToFirst()) {
         return map.load(db, valc);
       } else {
